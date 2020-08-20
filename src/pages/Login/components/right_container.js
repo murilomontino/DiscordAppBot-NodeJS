cf9f2ee40d2dc7c47bd64f26b4da0ce49032e316
Murilo Montino <murilomontinojr@hotmail.com>
2020-08-20 15:41:55 -0300

    arvore de projeto

---
+++ src/pages/Login/components/right_container.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Right_container = () => {
+    return (
+        <section className="right-container">
+            <div className="icon-right"></div>    
+            <div className="input-container">
+                <p>Entre com seu Token:</p>
+                <input type="text" name="name" />
+                <button>Entrar</button>
+            </div>
+
+        </section>
+    )
+}
+
+export default Right_container
