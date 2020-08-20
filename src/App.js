6f6a9e11ea0b02f6d04757cffde9c7e35c27e40f
Lucas Lima <lucas.alima@souunit.com.br>
2020-08-20 15:41:11 -0300

    fix container left

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import './assets/styles/global.css';
 
-import Login from './pages/Login'
-import Main from './pages/Main'
-import Navegador from './components/menuBar/Navegador'
+import Routes from "./routes";
 
 function App() {
   return (
-    
-      <BrowserRouter className='App'>
-         <Navegador/>
-          <Switch>
-            <Route path='/' exact component={Main} />
-            <Route path='/Login' component={Login} />
-          </Switch>
-      
-      </BrowserRouter>
+      <div className='App'>
+        <Routes/>
+      </div>
   )
 }
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -24,6 +24,7 @@ function Login() {
         <div className="icon-right"></div>
         <div className="input-container">
           <p>Entre com seu Token:</p>
+          <form action="http://localhost:3000/main"></form>
           <input type="text" name="name" />
           <button>Entrar</button>
         </div>
