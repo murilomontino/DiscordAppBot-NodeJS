1751e75cd2db5a674f86957b93c7f51405b50163
Murilo Montino <murilomontinojr@hotmail.com>
2020-08-22 19:53:17 -0300

    MenuNavegador

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Navegador from './components/menuBar'
+import Navegador from './components/MenuNavegador'
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 import './assets/styles/global.css';
