65ae0463ceaed130a04af5eb6303d51814dfae36
Murilo Montino <murilomontino@users.noreply.github.com>
2020-08-31 13:55:37 -0300

    folder comunication

--- app/comunication.js
+++ app/comunication.js
@@ -2,8 +2,8 @@ const Bot = require('./botDiscord/bot')
 const DiscordAppBot = new Bot()
 const config = require('./botDiscord/config/config.json')
 
-const {...comunicationGetSet} = require('./comunicationGetterSetter')(DiscordAppBot)
-const {...comunicationLoginLogout} = require('./comunicationLoginLogout')(DiscordAppBot, config)
+const {...comunicationGetSet} = require('./comunication/comunicationGetterSetter')(DiscordAppBot)
+const {...comunicationLoginLogout} = require('./comunication/comunicationLoginLogout')(DiscordAppBot, config)
 
 const Comunication = {
     ...comunicationLoginLogout,
