b741b26c191adcad9b601699789e5c0c64c867cf
Lucas Lima <lucas.alima@souunit.com.br>
2020-09-06 18:57:30 -0300

    merge

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -23,17 +23,17 @@ function Main() {
     <div className="background">
       <div className="menu-container">
 
-        <button className="item-menu">
+        <button className="item-menu" title="Perfil">
           <ProfileIcon className="icon-menu" />
         </button>
 
-        <button className="item-menu">
+        <button className="item-menu" title="Testes">
          
           <PlugueIcon className="icon-menu" />
         </button>
 
         <form action="/">
-        <button type="submit" className="item-menu">          
+        <button type="submit" className="item-menu"  title="Voltar">          
         <BackIcon className="icon-menu" />
         </button>
         </form>
