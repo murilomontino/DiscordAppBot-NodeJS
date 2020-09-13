02f7aa0fcdf50073ba04ff8ef9aed779b28e6f5f
Murilo Montino <murilomontinojr@hotmail.com>
2020-09-12 21:02:09 -0300

    Revert "guildCard color dark"
    This reverts commit e5426f0793bcb127e50063e14f12fbca61165993.

--- src/pages/Profile/components/guild_card.js
+++ src/pages/Profile/components/guild_card.js
@@ -2,15 +2,15 @@ import React from 'react';
 import "./guild_card.css";
 
 export default () => {
-    return (<>
-        <div className="guild">
-            <div className="guild-icon"></div>
-            <div className="guild-name-container "> Guild Name Here</div>
-            <div className="guild-informations ">
-                <p>Membros: 10.000</p>
-                <p>Membros Online: 666</p>
-                <p>Criador: Meliro</p>
-            </div>
-        </div>
-    </>)
+return(<> 
+<div className="guild"> 
+<div className="guild-icon"></div>
+<div className="guild-name-container"> Guild Name Here</div>
+<div className="guild-informations"> 
+<p>Membros: 10.000</p>
+<p>Membros Online: 666</p>
+<p>Criador: Meliro</p>
+</div>
+</div>
+</>)
 }
